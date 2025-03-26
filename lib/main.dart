029b53855c24b3d8a1e9d9b6805eaa2267e8f7a5
Aurelio Borges <aurelioaborges@gmail.com>
2025-03-26 11:12:23 -0300

    Add questions and buttons to main app interface

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,23 @@ class QuestionsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello, Flutter!'));
+    final questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Questions')),
+        body: Column(
+          children: <Widget>[
+            Text(questions[0]),
+            ElevatedButton(onPressed: null, child: Text('Answer 1')),
+            ElevatedButton(onPressed: null, child: Text('Answer 2')),
+            ElevatedButton(onPressed: null, child: Text('Answer 3')),
+          ],
+        ),
+      ),
+    );
   }
 }
