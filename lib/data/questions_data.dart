2775aeb590be5e2a7299e9d56d5c85a67747b4bb
Aurelio Borges <aurelioaborges@gmail.com>
2025-03-27 15:19:13 -0300

    feat: update questions data structure to include scoring for answers and add new questions

--- lib/data/questions_data.dart
+++ lib/data/questions_data.dart
@@ -2,19 +2,39 @@ export 'questions_data.dart';
 
 final questionsData = [
   {
-    'question': 'What\'s your favorite color?',
-    'answers': ['Black', 'Red', 'Green', 'White'],
-  },
-  {
-    'question': 'What\'s your favorite animal?',
-    'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    'question': 'What\'s your favorite music genre?',
+    'answers': [
+      {'text': 'Hardcore', 'score': 7},
+      {'text': 'Rock n\' Roll', 'score': 4},
+      {'text': 'Heavy Metal', 'score': 9},
+      {'text': 'Punk Rock', 'score': 6},
+    ],
   },
   {
     'question': 'What\'s your favorite food?',
-    'answers': ['Pizza', 'Pasta', 'Burger', 'Noodles'],
+    'answers': [
+      {'text': 'Pizza', 'score': 6},
+      {'text': 'Pasta', 'score': 7},
+      {'text': 'Burger', 'score': 4},
+      {'text': 'Barbecue', 'score': 9},
+    ],
   },
   {
     'question': 'What\'s your favorite programming language?',
-    'answers': ['Python', 'Java', 'Dart', 'JavaScript'],
+    'answers': [
+      {'text': 'Python', 'score': 8},
+      {'text': 'Java', 'score': 5},
+      {'text': 'Dart', 'score': 10},
+      {'text': 'JavaScript', 'score': 6},
+    ],
+  },
+  {
+    'question': 'What\'s your favorite framework?',
+    'answers': [
+      {'text': 'React Native', 'score': 5},
+      {'text': 'Flutter', 'score': 8},
+      {'text': 'Svelte', 'score': 3},
+      {'text': 'Vue', 'score': 10},
+    ],
   },
 ];
