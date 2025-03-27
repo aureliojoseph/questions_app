764948bf0a170103587a7967332955c942df24fc
Aurelio Borges <aurelioaborges@gmail.com>
2025-03-27 15:18:44 -0300

    feat: enhance ResultsWidget to display score-based feedback and total score

--- lib/widgets/results_widget.dart
+++ lib/widgets/results_widget.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 
 class ResultsWidget extends StatelessWidget {
-  const ResultsWidget(this.text, {super.key});
-  final String text;
+  const ResultsWidget(this.totalScore, {super.key});
+  final int totalScore;
+
+  String get resultPhrase {
+    String resultText;
+    if (totalScore < 20) {
+      resultText = 'So young and innocent! 🌟';
+    } else if (totalScore < 30) {
+      resultText = 'Pretty likeable! 😊';
+    } else if (totalScore < 40) {
+      resultText = 'You are... strange?! 🤔';
+    } else {
+      resultText = 'Maybe try new things?! 🌈';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        text,
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            resultPhrase,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'Your total score is: $totalScore',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.indigo.shade600,
+            ),
+          ),
+        ],
       ),
     );
   }
